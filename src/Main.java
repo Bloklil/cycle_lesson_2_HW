4560f70e0f9f6e247fe1b156c84ed0ca39da6910
Anton S <hitman.soldirs@gmail.com>
2025-03-18 16:05:35 +0800

    redone it 1 time task 2

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,26 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задание 1");
-        int deposit1 = 15000;
+        int maxSum = 2_459_000;
+        int deposit = 15000;
         int month = 0;
         int pocket = 0; // в кармане
-        while (pocket < 2_459_000) {
-            pocket += deposit1;
+        while (pocket < maxSum) {
+            pocket += deposit;
             month++;
             System.out.println("Месяц " + month + ", сумма накомлений равна " + pocket + " рублей.");
         }
         System.out.println("Задание 2");
-        int number2 = 0;
-        while (number2 < 10) {
-            number2++;
-            System.out.print(number2 + " ");
+        int number = 0;
+        while (number < 10) {
+            number++;
+            System.out.print(number + " ");
         }
         System.out.println(" ");
-        int number2_1 = 10;
-        while (number2_1 > 0) {
-            System.out.print(number2_1 + " ");
-            number2_1--;
+        int numberTwo = 10;
+        while (numberTwo > 0) {
+            System.out.print(numberTwo + " ");
+            numberTwo--;
         }
         System.out.println(" ");
         System.out.println("Задание 3");
@@ -34,37 +35,37 @@ public class Main {
             System.out.println("Год " + years + " , численность населения составляет " + population);
         }
         System.out.println("Задание 4");
-        int replenishment = 15000; //пополнение
-        int pocket_1 = 0; //на данный момент
+        deposit = 15000; //пополнение
+        pocket = 0; //на данный момент
         float percent = 0.07f;// процент
-        int month_1 = 0;
-        while (pocket_1 < 12_000_000) {
-            month_1++;
-            pocket_1 += replenishment + (pocket_1 + replenishment) * percent;
-            System.out.println("За " + month_1 + " месяц сумма накоплений равна " + pocket_1 + " рублей.");
+        month = 0;
+        while (pocket < 12_000_000) {
+            month++;
+            pocket += deposit + (pocket + deposit) * percent;
+            System.out.println("За " + month + " месяц сумма накоплений равна " + pocket + " рублей.");
         }
         System.out.println("Задание 5");
-        int deposit = 15000;   //вклад
-        int onMyHands = 0;    //на руках
-        float bet = 0.07f;    //ставка
+        deposit = 15000;
+        pocket = 0;
+        percent = 0.07f;
         int monthDeposit = 0;
-        while (onMyHands < 12_000_000) {
+        while (pocket < 12_000_000) {
             monthDeposit++;
-            onMyHands += deposit + (onMyHands + deposit) * bet;
+            pocket += deposit + (pocket + deposit) * percent;
             if (monthDeposit % 6 == 0) {
-                System.out.println("За " + monthDeposit + " месяц сумма накоплений равна " + onMyHands + " рублей.");
+                System.out.println("За " + monthDeposit + " месяц сумма накоплений равна " + pocket + " рублей.");
             }
         }
         System.out.println("Задание 6");
-        int deposit6 = 15000;   //вклад
-        int onMyHands6 = 0;    //на руках
-        float bet6 = 0.07f;    //ставка
-        int monthDeposit6 = 0;
-        while (monthDeposit6 < 108) {
-            monthDeposit6++;
-            onMyHands6 += deposit6 + (onMyHands6 + deposit6) * bet6;
-            if (monthDeposit6 % 6 == 0) {
-                System.out.println("За " + monthDeposit6 + " месяц сумма накоплений равна " + onMyHands6 + " рублей.");
+        deposit = 15000;
+        pocket = 0;
+        percent = 0.07f;
+        monthDeposit = 0;
+        while (monthDeposit < 108) {
+            monthDeposit++;
+            pocket += deposit + (pocket + deposit) * percent;
+            if (monthDeposit % 6 == 0) {
+                System.out.println("За " + monthDeposit + " месяц сумма накоплений равна " + pocket + " рублей.");
             }
         }
         System.out.println("Задание 7");
@@ -77,9 +78,10 @@ public class Main {
         int thisYear = 2025;
         int lastYear = thisYear - 200;
         int nextYear = thisYear + 100;
-        while (lastYear < nextYear) {
-            System.out.println(lastYear);
-            lastYear += 79;
-        }
+        for (int i = 0; i < nextYear; i = i + 79)
+            if (i > lastYear && i < nextYear) {
+                System.out.println(i);
+            }
+
     }
 }
